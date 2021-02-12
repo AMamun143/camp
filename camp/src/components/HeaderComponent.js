d789c5772f1d0568e5e4f8d20011405a05181dc4
Danielle Robitshek <drobi710@gmail.com>
2021-02-12 15:32:00 -0500

    add HeaderComponent

--- camp/src/components/HeaderComponent.js
+++ camp/src/components/HeaderComponent.js
@@ -1,1 +1,15 @@
-import React, { Component } from 'react';
+import { React } from 'react';
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>Header</div>
+        );
+    }
+}
+
+export default Header;
