8a195616eea64f38e7924ffcacc6db95823a71ec
Danielle Robitshek <drobi710@gmail.com>
2021-02-12 15:26:23 -0500

    folder and file skeleton

---
+++ camp/src/components/AboutComponent.js
@@ -0,0 +1,15 @@
+import { React } from 'react';
+
+class About extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>About</div>
+        );
+    }
+}
+
+export default About;
---
+++ camp/src/components/ContactComponent.js
@@ -0,0 +1,15 @@
+import { React } from 'react';
+
+class Contact extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>Contact</div>
+        );
+    }
+}
+
+export default Contact;
