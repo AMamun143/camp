b2e0da227b5713b32728801e6e21ca520484000b
Danielle Robitshek <drobi710@gmail.com>
2021-02-27 21:13:28 -0500

    update hero

--- camp/src/components/Hero/Hero.js
+++ camp/src/components/Hero/Hero.js
@@ -1,15 +1,27 @@
 import React from 'react'
-import '../Hero/Hero.css';
+import styles from '../Hero/Hero.module.css';
+import { Link } from 'react-router-dom';
 
 function Hero() {
     return (
-        <div className="hero">
-           <h1 className="header">PLAN YOUR ADVENTURE</h1> 
-           <h3 className="shop">SHOP NOW</h3>
-           <div className="arrow">
-           <i class="fas fa-chevron-down"></i>
+        <>
+        <Link className={styles.link_hero} to="/shop">
+        <div className={styles.hero}>
+           
+           <h1 className={styles.header_hero}>PLAN YOUR ADVENTURE</h1> 
+           
+           <h3 className={styles.shop}>SHOP NOW</h3>
+           
+           
+           <div className={styles.arrow}>
+          
+           <i class="fas fa-chevron-down" />
+          
            </div>
+         
         </div>
+        </Link>
+        </>
     )
 }
 
