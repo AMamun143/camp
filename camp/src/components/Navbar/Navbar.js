96bd4d3c3df193923233d430ef7953ccb0ebf15d
Mac <jerrsiphavong@icloud.com>
2021-03-02 18:39:09 -0500

    product and cart count fix

--- camp/src/components/Navbar/Navbar.js
+++ camp/src/components/Navbar/Navbar.js
@@ -2,60 +2,67 @@ import React, { Component } from "react";
 import { MenuItems } from "./MenuItems";
 import { Button } from "../Navbar/Button.js";
 import "../Navbar/Navbar.css";
-import { Link } from 'react-router-dom';
-import {SignInModal} from '../../pages/signIn/SignInComponent'
+import { Link } from "react-router-dom";
+import { SignInModal } from "../../pages/signIn/SignInComponent";
+import {DataContext } from '../../pages/shop/Context'
 
 class Navbar extends Component {
-  state = { 
+  static contextType = DataContext;
+  state = {
     clicked: false,
-    addModalShow: false
-   }
+    addModalShow: false,
+  };
 
   handleClick = () => {
-    
-    this.setState({ clicked: !this.state.clicked })
-  }
+    this.setState({ clicked: !this.state.clicked });
+  };
 
   render() {
-    let addModalClose = () => this.setState({addModalShow: false});
+    const {cart} = this.context;
+    let addModalClose = () => this.setState({ addModalShow: false });
     return (
       <>
-      <nav className="NavbarItems">
-        <h1 className="navbar-logo">
-          <pre className="pre">CA</pre>MP
-        </h1>
-        <div className="menu-icon" onClick={this.handleClick}>
-          <i
-            className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}
-          ></i>
-        </div>
-        <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-          {MenuItems.map((item, index) => {
-            return (
-              <li key={index}>
-                <a className={item.cName} href={item.url}>
-                  {item.title}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-        <div className="buttons">
-        <Button onClick={() => this.setState({addModalShow: true})} className="user">
-        <i class="fas fa-user" ></i>
-        </Button>
-        <SignInModal
-           show={this.state.addModalShow}
-           onHide={addModalClose} />
-        <Link to='/cart'>
-        <Button className="cart">
-        <i class="fas fa-shopping-cart" ></i>
-        </Button>
+        <nav className="NavbarItems">
+          <h1 className="navbar-logo">
+            <pre className="pre">CA</pre>MP
+          </h1>
+          <div className="menu-icon" onClick={this.handleClick}>
+            <i
+              className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+            ></i>
+          </div>
+          <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+            {MenuItems.map((item, index) => {
+              return (
+                <li key={index}>
+                  <a className={item.cName} href={item.url}>
+                    {item.title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+          <div className="buttons">
+            <Button
+              onClick={() => this.setState({ addModalShow: true })}
+              className="user"
+            >
+              <i class="fas fa-user"></i>
+            </Button>
+            <SignInModal
+              show={this.state.addModalShow}
+              onHide={addModalClose}
+            />
+           
+            <Link to="/cart">
+              <Button className="cart">
+                <i class="fas fa-shopping-cart"></i>
+              </Button>
 
-        </Link>
-        </div>
-      </nav>
-
+            </Link>
+            <span className="number">{cart.length}</span>
+          </div>
+        </nav>
       </>
     );
   }
--- camp/src/pages/cart/Products.js
+++ camp/src/pages/cart/Products.js
@@ -4,39 +4,38 @@ import { DataContext } from "../shop/Context";
 import "../cart/Products.css";
 
 export class Products extends Component {
-
     static contextType = DataContext;
-
+  
     render() {
-        const {products,addCart} = this.context;
-        return (
-            <div id="product">
-               {
-                   products.map(product =>(
-                       
-                      
-                       <div className="card" key={product._id}>
-                           
-                               <img src={product.src} alt=""/>
-                          
-                           <div className="content">
-                               <h3>
-                                   {product.title}
-                               </h3>
-                              
-                               <p>{product.description}</p>
-                               <span>${product.price}</span>
-                               <button onClick={()=> addCart(product._id)}>Add to cart</button>
-                             
-                           </div>
-                          
-                       </div>
-                       
-                   ))
-               }
+      const { products, addCart } = this.context;
+      return (
+        <>
+          <h1 id="title">Shop Camping & Hiking Gear</h1>
+          <h4 id="pageDesc">
+            From overnighting to day camping CAMP can get you the right gear to
+            get you out there in no time.
+          </h4>
+          <div id="product">
+            <div id="productWrapper">
+              {products.map((product) => (
+                <div className="card" key={product._id}>
+                  <img src={product.src} alt="" />
+  
+                  <div className="content">
+                    <h3 id="productTitle">{product.title}</h3>
+                    <p id="productDescription">{product.description}</p>
+                    <p id="productPrice">${product.price}</p>
+                    <button onClick={() => addCart(product._id)}>
+                      Add to cart
+                    </button>
+                  </div>
+                </div>
+              ))}
             </div>
-        )
+          </div>
+        </>
+      );
     }
-}
-
-export default Products
+  }
+  
+  export default Products;
